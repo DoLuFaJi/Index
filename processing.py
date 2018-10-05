f7c94f1106438af62cc14adf3d0ec26f02ab38ff
shikshak <ft.mezid@gmail.com>
2018-10-05 17:04:52 +0000

    score, mom class

--- processing.py
+++ processing.py
@@ -3,11 +3,18 @@ from settings import DATAFOLDER
 
 def tokenization (file_name):
     #This method takes the name of the document and return the list of strings
+    pass
 
 class Scoring:
     def __init__(self):
         #Constructor
         pass
 
-    def scoring (self, file_name, term):
-        #This method ''
+class FrequencyScoring(Scoring):
+    def __init__(self):
+        #Constructor
+        pass
+
+    def score (self, file_name, term):
+        #This method use the term frequency to give a score to a term
+        pass
