3de72518d8de2d229693b9a19d75d8c0fb06a365
Lucas Marie <lucas.marie@insa-lyon.fr>
2018-10-12 16:04:47 +0200

    Fixed some typos, started if

--- indexing.py
+++ indexing.py
@@ -19,6 +19,11 @@ class Vocabulary:
     def __eq__(self, other):
         return self.term == other.term
 
+    def __str__(self):
+        return self.term
+
+    def __repr__(self):
+        return self.term
 
 def flush_on_disk(inverted_file, posting_lists):
     pickle.dump(inverted_file, open('if.p', 'wb'))
@@ -29,21 +34,36 @@ def flush_on_disk(inverted_file, posting_lists):
 # resource.setrlimit(resource.RLIMIT_AS, (RAM_LIMIT_MB*1024, RAM_LIMIT_MB*1024))
 
 # posting_lists = mmap.mmap(-1, RAM_LIMIT_MB*1024)
+
+#compute toi meme la frequency
+
 posting_lists = []
 vocabulary_set = {}
 try:
     score_calculator = Scoring()
-    for filename in os.listdir(TEST_DATAFOLDER):
+    list_documents = os.listdir(TEST_DATAFOLDER)
+    nb_documents = len(list_documents)
+    for filename in list_documents:
         terms = tokenization(filename)
         print(str(len(terms)) + ' terms to process')
         for term in terms:
             vocabulary = Vocabulary(term)
             if vocabulary not in vocabulary_set:
-                vocabulary_set[vocabulary] = 1
-            posting_lists.append((filename, 0))
+                vocabulary_set[vocabulary] = []
+            print('frequency')
+            score = score_calculator.__tf__(terms, term)
+            print('frequency done')
+            print('append')
+            vocabulary_set[vocabulary].append((filename, score))
+            print('append done')
             vocabulary.posting_list_size += 1
         print(filename + ' done')
 
+    for vocabulary, posting_lists in vocabulary_set.items():
+        for document, score in posting_lists:
+            print('idf')
+            score *= score_calculator.__idf__(vocabulary.posting_list_size, nb_documents)
+            print('idf done')
     pprint.pprint(vocabulary_set)
 
 except MemoryError:
--- processing.py
+++ processing.py
@@ -24,13 +24,13 @@ def tokenization (file_name):
 
 class Scoring:
     #Class to get score for term
-    def __tf__ (self, tokens, term):
+    def __tf__(self, tokens, term):
         #This method use the term frequency inside the file to give a score to a term
         #tokens = tokenization(file_name)
-        tf = math.log10( 1+( tokens.count(term) / len(tokens) ) )
+        tf = math.log10(1+(tokens.count(term) / len(tokens)))
         return tf
 
-    def __idf__ (self, nb_documents_term_appears, number_of_documents):
+    def __idf__(self, nb_documents_term_appears, number_of_documents):
         #This method use the inverse document frequency to give a score to a term
 
         #number of documents where the term appears
@@ -39,10 +39,10 @@ class Scoring:
         #    if self.__tf__(file, term)>0:
         #        nb_documents_term_appears += 1
 
-        idf = math.log10( number_of_documents / (1+nb_documents_term_appears) )
+        idf = math.log10(number_of_documents / (1+nb_documents_term_appears))
         return idf
 
-    def compute_score (self, tokens, term, nb_documents_term_appears, number_of_documents):
+    def compute_score(self, tokens, term, nb_documents_term_appears, number_of_documents):
         #compute the score of a term in a document
         tf = self.__tf__(tokens, term)
         idf = self.__idf__(nb_documents_term_appears, number_of_documents)
@@ -50,7 +50,7 @@ class Scoring:
         return score
 
 #test
-lolo = Scoring()
-for file in os.listdir(DATAFOLDER):
-    tokfile = tokenization(file)
-    lili = lolo.compute_score(tokens=tokfile, term="alien", nb_documents_term_appears=359, number_of_documents=732)
+# lolo = Scoring()
+# for file in os.listdir(DATAFOLDER):
+#     tokfile = tokenization(file)
+#     lili = lolo.compute_score(tokens=tokfile, term="alien", nb_documents_term_appears=359, number_of_documents=732)
